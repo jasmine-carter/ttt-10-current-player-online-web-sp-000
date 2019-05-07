71186dfdf7039ea775eb2a498c116fae2f7f8bff
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-07 00:04:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 counter = 0
 def turn_count(board)
   board.each do |space|
-      if space == "X"
-        counter = 1
+      if space == "X" || space == "O"
+        counter += 1
       end
     end
 end
